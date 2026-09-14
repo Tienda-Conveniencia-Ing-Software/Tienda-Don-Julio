db72010d3823830c9a68b3a299e9e30985274221
Valentina Gutierrez <idkp0987@gmail.com>
2026-09-14 11:24:18 -0500

    Update InventoryMenu.cs
    corrección: mostrar precio de compra en el menú de inventario

--- TiendaBarrio/UI/InventoryMenu.cs
+++ TiendaBarrio/UI/InventoryMenu.cs
@@ -1,26 +1,19 @@
-namespace TiendaBarrio.Utils;
-
-using TiendaBarrio.Core.Models;
-using TiendaBarrio.Persistence;
-public class ShowProducts()
+public void StockMenu(List<Product> products)
 {
-    public void StockMenu(List<Product> products)
+    try
     {
-        try
+        Console.WriteLine("== products ==");
+        foreach (Product p in products)
         {
-
-            Console.WriteLine("== products ==");
-            foreach (Product p in products)
-            {
-                Console.Write($"[{p.ID}]");
-                Console.Write(p.Name);
-                Console.Write(p.Price + "$ ");
-                Console.WriteLine(p.Stock);
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Exception: " + e.Message);
+            Console.Write($"[{p.ID}] ");
+            Console.Write(p.Name);
+            Console.Write(" | Sale: " + p.Price + "$ ");
+            Console.Write("| Purchase: " + p.PurchasePrice + "$ ");
+            Console.WriteLine("| Stock: " + p.Stock);
         }
     }
- }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+}
